198dfc8f9580106afc2a6adc2348ecdb675de2dc
SinoharaRyuga <raitan3170@gmail.com>
2022-07-02 15:36:39 +0900

    フリック入力を出来るようにした　シーンを変更出来る機能を作成

--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -6,14 +6,31 @@ public class InputTest : MonoBehaviour
 {
     [SerializeField, Tooltip("タッチされている場所を示すオブジェクト")] GameObject _touchObjectPrefab = default;
     [SerializeField, Tooltip("タップされた回数を表示　デバッグも表示する ")] TextMeshProUGUI _tapCountText = default;
+    [SerializeField, Tooltip("フリック入力テストする為オブジェクト")] GameObject _flickTestObject = default;
     [SerializeField, Tooltip("デバッグ情報を表示")] TextMeshProUGUI _debugText = default;
     [SerializeField, Tooltip("ボタンが押された時のテキスト")] TextMeshProUGUI _buttonDebugText = default;
+    [SerializeField, Tooltip("フリック入力を受け付ける最小の移動距離")] float _minInputFlickDirection = 20f;
+    [SerializeField, Tooltip("フリック入力を受け付ける最大の移動距離")] float _maxInputFlickDirection = 50f;
+    [SerializeField, Tooltip("フリックが反応するまでの時間")] float _activateFlickTime = 1f;
+
     /// <summary>Instantiateしたクローン</summary>
     GameObject _touchObject = default;
     /// <summary>タップ回数 </summary>
     int _tapCount = 0;
+    /// <summary>_flickTestObjectのrotate.zの値 </summary>
+    int _flickRotateValue = 0;
     /// <summary>前回タッチされたXの位置 </summary>
     float _lasttouchpositionX = 0f;
+    /// <summary>フリックでのX移動距離 </summary>
+    float _flickDirectionX = 0f;
+    /// <summary>フリックでのY移動距離 </summary>
+    float _flickDirectionY = 0f;
+    /// <summary>画面に指が触れてからの時間　フリックが反応する時間</summary>
+    float _flickTime = 0f;
+    /// <summary>フリックが反応する時間の計測を開始 </summary>
+    bool _isflickTimer = false;
+    Vector3 _startPoint = Vector3.zero;
+    Vector3 _endPoint = Vector3.zero;
     void Start()
     {
         _touchObject = Instantiate(_touchObjectPrefab, Vector2.zero, Quaternion.identity);
@@ -21,27 +38,74 @@ public class InputTest : MonoBehaviour
 
     void Update()
     {
-        foreach (var touch in Input.touches)
+        //foreach (var touch in Input.touches)
+        //{
+        //    var touchposition = new Vector3(touch.position.x, touch.position.y, 10);
+        //    touchposition = Camera.main.ScreenToWorldPoint(touchposition);
+
+        //    if (touch.phase == TouchPhase.Moved)
+        //    {
+        //        _touchObject.transform.position = touchposition;
+
+        //        if (touch.position.x < _lasttouchpositionX) //左に移動
+        //        {
+        //            _debugText.text = "左に移動";
+        //        }
+        //        else if (touch.position.x > _lasttouchpositionX) //右に移動
+        //        {
+        //            _debugText.text = "右に移動";
+        //        }
+
+        //        _tapCountText.text = $"{touch.position.x} {_lasttouchpositionX}";   //x位置を表示
+        //        _lasttouchpositionX = touch.position.x;
+        //    }
+        //}
+
+        foreach (var touch in Input.touches)    //フリック入力
         {
-            var touchposition = new Vector3(touch.position.x, touch.position.y, 10);
-            touchposition = Camera.main.ScreenToWorldPoint(touchposition);
-
-            if (touch.phase == TouchPhase.Moved)
+            if (touch.phase == TouchPhase.Began)
             {
-                _touchObject.transform.position = touchposition;
-
-                if (touch.position.x < _lasttouchpositionX) //左に移動
-                {
-                    _debugText.text = "左に移動";
-                }
-                else if (touch.position.x > _lasttouchpositionX) //右に移動
-                {
-                    _debugText.text = "右に移動";
-                }
-
-                _tapCountText.text = $"{touch.position.x} {_lasttouchpositionX}";   //x位置を表示
-                _lasttouchpositionX = touch.position.x;
+                _startPoint = new Vector3(touch.position.x, touch.position.y, 10);
+                _isflickTimer = true;
             }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                _endPoint = new Vector3(touch.position.x, touch.position.y, 10);
+                GetFlickDistance();
+                _buttonDebugText.text = $"移動距離 X{_flickDirectionX} Y{_flickDirectionY}";
+               
+                //フリック入力が出来ているかどうかの判定
+                if (_minInputFlickDirection <= _flickDirectionX && _flickDirectionX <= _maxInputFlickDirection ||
+                    _minInputFlickDirection <= _flickDirectionY && _flickDirectionY <= _maxInputFlickDirection &&
+                    _flickTime <= _activateFlickTime)
+                {
+                    _flickRotateValue += 90;
+                    _flickTestObject.transform.Rotate(0, 0, _flickRotateValue);
+
+                    if (360 <= _flickRotateValue)
+                    {
+                        _flickRotateValue = 0;
+                    }
+
+                    _debugText.text = "フリック成功";
+                }
+                else
+                {
+                    _debugText.text = "フリック失敗";
+                }
+
+                _startPoint = Vector3.zero;
+                _endPoint = Vector3.zero;
+                _flickTime = 0f;
+                _isflickTimer = false;
+            }
+        }
+
+        if (_isflickTimer)
+        {
+            _flickTime += Time.deltaTime;
+            _tapCountText.text = _flickTime.ToString();
         }
     }
 
@@ -51,9 +115,17 @@ public class InputTest : MonoBehaviour
         _buttonDebugText.text = "ボタンが押された";
     }
 
+    /// <summary>ボタンの機能を試す為の関数 </summary>
     public void ResetButton()
     {
         _buttonDebugText.text = "リセット";
+    }
+
+    /// <summary>フリックの距離を求める </summary>
+    void GetFlickDistance()
+    {
+        _flickDirectionX = Mathf.Abs(_endPoint.x - _startPoint.x);
+        _flickDirectionY = Mathf.Abs(_endPoint.y - _startPoint.y);
     }
 }
 
--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -5,8 +5,25 @@ public class WallController : MonoBehaviour
 {
     [SerializeField, Tooltip("回転速度")] float _rotateSpeed = 0.1f;
     [SerializeField, Tooltip("回転方向")] RotateDirection _rotateDirection = RotateDirection.Normal;
+    [SerializeField, Tooltip("フリック入力を受け付ける最小の移動距離")] float _minInputFlickDirection = 20f;
+    [SerializeField, Tooltip("フリック入力を受け付ける最大の移動距離")] float _maxInputFlickDirection = 50f;
+    [SerializeField, Tooltip("フリックが反応するまでの時間")] float _activateFlickTime = 1f;
+    /// <summary>画面に指が触れた時の場所 </summary>
+    Vector3 _startPoint = Vector3.zero;
+    /// <summary>画面に指が離れた時の場所 </summary>
+    Vector3 _endPoint = Vector3.zero;
+    /// <summary>_flickTestObjectのrotate.zの値 </summary>
+    int _flickRotateValue = 0;
+    /// <summary>フリックでのX移動距離 </summary>
+    float _flickDirectionX = 0f;
+    /// <summary>フリックでのY移動距離 </summary>
+    float _flickDirectionY = 0f;
     /// <summary>前回タッチされたXの位置 </summary>
     float _lasttouchpositionX = 0f;
+    /// <summary>画面に指が触れてからの時間　フリックが反応する時間</summary>
+    float _flickTime = 0f;
+    /// <summary>フリックが反応する時間の計測を開始 </summary>
+    bool _isflickTimer= false;
 
     public float RotateSpeed
     {
@@ -33,7 +50,7 @@ public class WallController : MonoBehaviour
             ChangeMoveDirection(false);
         }
 
-        foreach (var touch in Input.touches)
+        foreach (var touch in Input.touches)    //スワイプ入力
         {
             if (touch.phase == TouchPhase.Moved)
             {
@@ -48,6 +65,46 @@ public class WallController : MonoBehaviour
 
                 _lasttouchpositionX = touch.position.x;
             }
+        }
+
+        foreach (var touch in Input.touches)    //フリック入力
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                _startPoint = new Vector3(touch.position.x, touch.position.y, 10);
+                _isflickTimer = true;
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                _endPoint = new Vector3(touch.position.x, touch.position.y, 10);
+                GetFlickDistance();
+            
+                //フリック入力が出来ているかどうかの判定
+                if (_minInputFlickDirection <= _flickDirectionX && _flickDirectionX <= _maxInputFlickDirection      //X軸のフリック
+                    || _minInputFlickDirection <= _flickDirectionY && _flickDirectionY <= _maxInputFlickDirection   //Y軸のフリック
+                    && _flickTime <= _activateFlickTime)   //フリックが発動するかどうか
+                {
+                    _flickRotateValue += 90;
+                     transform.Rotate(0, 0, _flickRotateValue);
+
+                    if (360 <= _flickRotateValue)
+                    {
+                        _flickRotateValue = 0;
+                    }
+
+                    _flickTime = 0f;
+                    _isflickTimer = false;
+                }
+               
+                _startPoint = Vector3.zero;
+                _endPoint = Vector3.zero;
+            }
+        }
+
+        if (_isflickTimer)
+        {
+            _flickTime += Time.deltaTime;
         }
     }
 
@@ -86,7 +143,13 @@ public class WallController : MonoBehaviour
         Debug.Log("リセット");
     }
 
-    
+    /// <summary>フリックの距離を求める </summary>
+    void GetFlickDistance()
+    {
+        _flickDirectionX = Mathf.Abs(_endPoint.x - _startPoint.x);
+        _flickDirectionY = Mathf.Abs(_endPoint.y - _startPoint.y);
+    }
+
 
     enum RotateDirection
     {
